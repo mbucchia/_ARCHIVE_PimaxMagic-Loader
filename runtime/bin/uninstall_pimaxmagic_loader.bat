ba8ba27a56d1ec4a62a39c472b55083326585e85
Matthieu Bucchianeri <mbucchia@gmail.com>
2023-09-24 18:11:12 -0700

    Be robust to SteamVR updates.

--- runtime/bin/uninstall_pimaxmagic_loader.bat
+++ runtime/bin/uninstall_pimaxmagic_loader.bat
@@ -1,8 +1,14 @@
 @echo off
 echo Reverting to original vrclient_x64.dll...
 if exist real_vrclient_x64.dll (
-	del vrclient_x64.dll
-	ren real_vrclient_x64.dll vrclient_x64.dll
+	fc /b our_vrclient_x64.dll vrclient_x64.dll > nul 2>&1
+	if errorlevel 1 (
+		echo Detected changes to vrclient_x64.dll... Assuming SteamVR was updated.
+		del real_vrclient_x64.dll
+	) else (
+		del vrclient_x64.dll
+		ren real_vrclient_x64.dll vrclient_x64.dll
+	)
 ) else (
 	echo NOTHING TO UNINSTALL
 )
